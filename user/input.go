321c2da49b07b76e9e18dc9353921e264024126a
dyaksaa_ <diasnour0395@gmail.com>
2021-12-05 23:56:50 +0700

    declared input login

--- user/input.go
+++ user/input.go
@@ -7,3 +7,8 @@ type RegisterUserInput struct {
 	Role       string `json:"role" binding:"required"`
 	Password   string `json:"password" binding:"required"`
 }
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
