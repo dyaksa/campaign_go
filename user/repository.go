9ebd756e718e63fad26b87f5f5f5e8f6ac73d098
dyaksaa_ <diasnour0395@gmail.com>
2021-12-07 12:41:45 +0700

    create repository udpate avatar file

--- user/repository.go
+++ user/repository.go
@@ -5,6 +5,8 @@ import "gorm.io/gorm"
 type Repository interface {
 	Save(user User) (User, error)
 	FindByEmail(email string) (User, error)
+	FindById(ID int) (User, error)
+	SaveAvatar(user User) (User, error)
 }
 
 type repository struct {
@@ -26,6 +28,23 @@ func (r *repository) Save(user User) (User, error) {
 func (r *repository) FindByEmail(email string) (User, error) {
 	var user User
 	err := r.db.Where("email = ?", email).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (r *repository) FindById(ID int) (User, error) {
+	var user User
+	err := r.db.Where("id = ?", ID).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (r *repository) SaveAvatar(user User) (User, error) {
+	err := r.db.Save(&user).Error
 	if err != nil {
 		return user, err
 	}
