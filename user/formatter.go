c0e1a02ab2014b82aa8e85614ddb6d0127008323
dyaksaa_ <diasnour0395@gmail.com>
2022-01-05 14:06:37 +0700

    add avatar filename formatter

--- user/formatter.go
+++ user/formatter.go
@@ -4,6 +4,7 @@ type Formatter struct {
 	ID         int    `json:"id"`
 	Name       string `json:"name"`
 	Occupation string `json:"occupation"`
+	ImgeURL    string `json:"image_url"`
 	Email      string `json:"email"`
 	Token      string `json:"token"`
 }
@@ -13,6 +14,7 @@ func FormatUser(user User, token string) Formatter {
 		ID:         user.ID,
 		Name:       user.Name,
 		Occupation: user.Occupation,
+		ImgeURL:    user.AvatarFileName,
 		Email:      user.Email,
 		Token:      token,
 	}
